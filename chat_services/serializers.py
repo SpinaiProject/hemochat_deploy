88afd4a0a61bcd273b06feea776999bb390e3ae4
hdsung98 <hdsung98@naver.com>
2024-02-12 00:00:21 +0900

    [Feat #2] ChatRoom Serializers 구현

---
+++ chat_services/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import ChatRoom
+import json
+
+
+class ChatRoomSerializer(serializers.ModelSerializer):
+    recent_chat_history = serializers.SerializerMethodField()
+    summarized_chat_history = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ChatRoom
+        fields = ['id', 'user', 'recent_chat_history', 'summarized_chat_history']
+
+    def get_recent_chat_history(self, obj):
+        return json.loads(obj.chat_history)
+
+    def get_summarized_chat_history(self, obj):
+        return json.loads(obj.summarized_chat_history)
