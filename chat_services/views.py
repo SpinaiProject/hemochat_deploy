aa9060a2a51e98dbcaed991856614724aae8c15e
hdsung98 <hdsung98@naver.com>
2024-02-13 23:36:15 +0900

    [Fix #2] create_stream 로직 변경 - chat_history를 리퀘스트 바디로 부터 받는 것으로 전제를 변경하고 이 정보를 질문api에 얹음

--- chat_services/views.py
+++ chat_services/views.py
@@ -156,7 +156,7 @@ def create_message(request):
 
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated]) # 어느 검사지에 대해 채팅할 것인지에 대한 정보 추가 필요
+@permission_classes([IsAuthenticated])  # 어느 검사지에 대해 채팅할 것인지에 대한 정보 추가 필요
 def create_chatroom(request):
     user = request.user
     chatroom = ChatRoom.objects.create(user=user)
@@ -194,21 +194,32 @@ def save_chat_history(chatroom_id, user_question, complete_answer):
 @permission_classes([IsAuthenticated])
 def create_stream(request, chatroom_id):
     try:
-        data = json.loads(request.body)
-        user_question = data.get('question', '')
-        previous_chats = data.get('previous_chats', '')
+        decoded_body = request.body.decode('utf-8')
+        data = json.loads(decoded_body)
+        user_question = data.get('user_question', '')
+        chat_history = data.get('chat_history', [])
     except json.JSONDecodeError:
         return JsonResponse({'error': 'Invalid JSON'}, status=400)
 
     api_key = os.environ.get('OPEN_AI_API_KEY')
     client = OpenAI(api_key=api_key)
+    system_feature = {
+        "role": "system",
+        "content": "의료 검사지에 대한 상담을 진행한다. 대화 기록을 토대로 \
+         이미 대화한 적 있는 내용에 대해서는 간단히 말해라. 반드시 한국말로 답변해라. 의료 상담과 무관한 분야에 대한 질문은 반드시 거절해라."
+    }
+
+    query = [system_feature]
+    query.extend(chat_history)
+    query.append({"role": "user", "content": user_question})
+    print(query)
     complete_answer = ""
 
     def event_stream():
         nonlocal complete_answer
         for chunk in client.chat.completions.create(
-                model="gpt-3.5-turbo-0613",
-                messages=[previous_chats, {"role": "user", "content": user_question}],
+                model="gpt-4-0613",
+                messages=query,
                 stream=True,
         ):
             chunk_message = chunk.choices[0].delta.content
@@ -217,7 +228,6 @@ def create_stream(request, chatroom_id):
                 save_chat_history(chatroom_id, user_question, complete_answer)
                 print(chatroom_id, user_question, complete_answer, "finished!")
                 break
-
             else:
                 complete_answer += chunk_message
             yield f"data: {chunk_message}\n\n"
