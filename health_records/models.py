1ab3d78a6d36f579fc455e1323875aa24b3b56c4
hdsung98 <hdsung98@naver.com>
2024-02-21 19:54:16 +0900

    [Fix #4] 검사지 폴더, 파일 모델 분리 - 각 파일은 하나의 상위 폴더내에 속하도록 함

--- health_records/models.py
+++ health_records/models.py
@@ -6,12 +6,17 @@ from users.models import User
 def user_directory_path(instance, filename):
     ext = filename.split('.')[-1]
     random_filename = get_random_string(20) + '.' + ext
-    return '{0}/{1}'.format(instance.user.random_directory_name, random_filename)
+    return '{0}/{1}/{2}'.format(instance.folder.user.random_directory_name, instance.folder.id, random_filename)
 
 
-class HealthRecord(models.Model):
+class HealthRecordFolder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class HealthRecordImage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    folder = models.ForeignKey(HealthRecordFolder, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to=user_directory_path)
     ocr_text = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
