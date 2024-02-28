d4fb4cc2138c8bd4823b1be2687779afa8bb6d12
hdsung98 <hdsung98@naver.com>
2024-02-28 19:50:18 +0900

    [Feat #7] 구글 로그인 관련 url 맵핑 - 기본 로그인 함수 및 콜백,종료 함수

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('kakao/logout/', kakao_logout, name='kakao_logout'),
     path('kakao/callback/', kakao_callback, name='kakao_callback'),
     path('kakao/login/finish/', KakaoLogin.as_view(), name='kakao_login_todoclass'),
+    path('google/login/', google_login, name='google_login'),
+    path('google/callback/', google_callback, name='google_callback'),
+    path('google/login/finish/', GoogleLogin.as_view(), name='google_login_todjango'),
 ]
