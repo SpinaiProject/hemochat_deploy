a2fc0d110aad6213f8f568eab1dea91cad35229f
hdsung98 <hdsung98@naver.com>
2024-03-08 01:49:07 +0900

    [Feat #8] 유저 모델 필드 확장 - 소셜로그인을 위해 플랫폼별로 카카오,구글이메일 필드 생성 - 나이,성별,생일,전화번호,인증번호 등 세부정보를 추가함

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
 import uuid
+import random
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.crypto import get_random_string
+from .utils import send_sms
 
 
 def generate_default_email():
@@ -10,9 +12,22 @@ def generate_default_email():
 
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=100)
-    kakao_email = models.EmailField(unique=True, default=generate_default_email)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+    nickname = models.CharField(max_length=100, null=True, blank=True)
+    kakao_email = models.EmailField(null=True, blank=True)
+    google_email = models.EmailField(null=True, blank=True)
     random_directory_name = models.CharField(max_length=20, unique=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    birth_year = models.PositiveIntegerField(null=True, blank=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    phone_verified = models.BooleanField(default=False)
+    verification_code = models.CharField(max_length=6, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.random_directory_name:
