69b6da353b045cb20e02296a03ec21995ca9ff07
hdsung98 <hdsung98@naver.com>
2024-03-08 16:01:16 +0900

    [Update #8] 채팅방에 health_record 세부정보 읽어오도록 시리얼라이저 업데이트

--- chat_services/serializers.py
+++ chat_services/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import ChatRoom
-import json
+from health_records.serializers import HealthRecordImageSerializer
 
 
 class ChatRoomSerializer(serializers.ModelSerializer):
     recent_chat_history = serializers.SerializerMethodField()
     summarized_chat_history = serializers.SerializerMethodField()
+    health_records = serializers.SerializerMethodField()
 
     class Meta:
         model = ChatRoom
@@ -13,9 +14,12 @@ class ChatRoomSerializer(serializers.ModelSerializer):
                   'entered', 'leaved']
 
     def get_recent_chat_history(self, obj):
-        # print(type(obj.chat_history), obj.chat_history)
         return obj.chat_history
 
     def get_summarized_chat_history(self, obj):
-        # print(type(obj.summarized_chat_history), obj.summarized_chat_history)
         return obj.summarized_chat_history
+
+    def get_health_records(self, obj):
+        health_records = obj.health_records.all()
+        serializer = HealthRecordImageSerializer(health_records, many=True)
+        return serializer.data
