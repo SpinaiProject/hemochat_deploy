cad617e6f0e4bbb564200d242cf0c00af4a21b6d
hdsung98 <hdsung98@naver.com>
2024-03-18 03:26:25 +0900

    test

--- health_records/views.py
+++ health_records/views.py
@@ -154,6 +154,12 @@ def general_ocr_analysis(request):
 
         response = requests.post(api_url, headers=headers, json=body)
         if response.status_code == 200:
+            # res = response.json()
+            # infer_texts_generator = extract_infer_text(res)
+            # infer_texts_list = list(infer_texts_generator)
+            # final_text = ' '.join(infer_texts_list)
+            # record.ocr_text = final_text
+            # record.save()
             res = response.json()
             record.ocr_text = json.dumps(res, ensure_ascii=False, indent=4)
             record.save()
