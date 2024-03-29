7b27e8a9dd66baa47fa7ce6aa1414e02c00a68b3
hdsung98 <hdsung98@naver.com>
2024-03-29 19:24:21 +0900

    [Fix #15] 불필요 URL 삭제

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('kakao/login/', kakao_login, name='kakao_login'),
     path('kakao/logout/', kakao_logout, name='kakao_logout'),
     path('kakao/callback/', kakao_callback, name='kakao_callback'),
-    path('kakao/login/finish/', KakaoLogin.as_view(), name='kakao_login_todoclass'),
+    # path('kakao/login/finish/', KakaoLogin.as_view(), name='kakao_login_todoclass'),
 
     path('google/login/', google_login, name='google_login'),
     path('google/callback/', google_callback, name='google_callback'),
