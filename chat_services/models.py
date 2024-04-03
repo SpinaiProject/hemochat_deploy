4160fac074ea10aad05b8b02c591de63a6dd4dde
hdsung98 <hdsung98@naver.com>
2024-04-03 12:11:04 +0900

    [Fix] 마이그레이션 에러 수정, assistant api 관련 모델 비활성화

--- chat_services/models.py
+++ chat_services/models.py
@@ -8,27 +8,27 @@ from django.db.models import Manager
 from hemochat_project import settings
 
 
-class AssistantConfig(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    api_key = models.CharField(max_length=255)
-    model = models.CharField(max_length=255, default='gpt-3.5-turbo')
-    active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-
-class ChatThread(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assistant_sessions')
-    thread_id = models.CharField(max_length=255, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects: Manager = models.Manager()
-    # title= models.CharField(max_length=255, blank=True)
-
+# class AssistantConfig(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=255)
+#     api_key = models.CharField(max_length=255)
+#     model = models.CharField(max_length=255, default='gpt-3.5-turbo')
+#     active = models.BooleanField(default=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class ChatThread(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assistant_sessions')
+#     thread_id = models.CharField(max_length=255, unique=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     objects: Manager = models.Manager()
+#     # title= models.CharField(max_length=255, blank=True)
+#
 
 # 아래는 Assistant API Streaming이 지원되지 않을 때 사용할 모델들
 class ChatRoom(models.Model):
