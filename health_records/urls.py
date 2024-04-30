cfd505ec6a075e4eca739bd8d0480385ea6a09ef
hdsung98 <hdsung98@naver.com>
2024-04-30 10:46:48 +0900

    [Feat #10] General OCR 분석과정 스트리밍

--- health_records/urls.py
+++ health_records/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('delete_health_records/', delete_health_records, name='delete_health_records'),
     path('user_health_records_count/', user_health_records_count, name='user_health_records_count'),
     path('general_ocr_analysis/', general_ocr_analysis, name='general_ocr_analysis'),
-    path('template_ocr_analysis/', template_ocr_analysis, name='template_ocr_analysis'),
-    path('template_ocr_analysis_for_gpt/', template_ocr_analysis_for_gpt, name='template_ocr_analysis_for_gpt'),
+    # path('template_ocr_analysis/', template_ocr_analysis, name='template_ocr_analysis'),
+    # path('template_ocr_analysis_for_gpt/', template_ocr_analysis_for_gpt, name='template_ocr_analysis_for_gpt'),
 ]
