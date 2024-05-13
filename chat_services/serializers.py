85b138eb6cab324f74eb961fd73dc896c22af31b
hdsung98 <hdsung98@naver.com>
2024-05-13 19:50:59 +0900

    [Fix #1] 채팅방 리스트 반환 API에 사용될 시리얼라이저 - 채팅방 시리얼라이저: 제목, 최종수정일자, 대표 썸네일 - 검사지 시리얼라이저: 이미지URL, ocr 분석결과, 생성일자

--- chat_services/serializers.py
+++ chat_services/serializers.py
@@ -1,25 +1,45 @@
 from rest_framework import serializers
-from .models import ChatRoom
+from .models import *
 from health_records.serializers import HealthRecordImageSerializer
 
 
-class ChatRoomSerializer(serializers.ModelSerializer):
-    recent_chat_history = serializers.SerializerMethodField()
-    summarized_chat_history = serializers.SerializerMethodField()
-    health_records = serializers.SerializerMethodField()
+class ChatRoomListSerializer(serializers.ModelSerializer):
+    representative_image = serializers.ImageField(source='health_records.first.image', read_only=True, allow_null=True)
 
     class Meta:
         model = ChatRoom
-        fields = ['id', 'user', 'health_records', 'recent_chat_history', 'summarized_chat_history', 'last_entered',
-                  'entered', 'leaved']
+        fields = ['chatroom_id', 'title', 'updated_at', 'representative_image']
 
-    def get_recent_chat_history(self, obj):
-        return obj.chat_history
+    def get_attribute(self, instance, attr):
+        try:
+            return super().get_attribute(instance, attr)
+        except AttributeError:
+            return None
 
-    def get_summarized_chat_history(self, obj):
-        return obj.summarized_chat_history
+class ChatRoomDetailSerializer(serializers.ModelSerializer):
+    health_records = HealthRecordImageSerializer(many=True)
 
-    def get_health_records(self, obj):
-        health_records = obj.health_records.all()
-        serializer = HealthRecordImageSerializer(health_records, many=True)
-        return serializer.data
+    class Meta:
+        model = ChatRoom
+        fields = '__all__'
+
+# class ChatRoomSerializer(serializers.ModelSerializer):
+#     recent_chat_history = serializers.SerializerMethodField()
+#     summarized_chat_history = serializers.SerializerMethodField()
+#     health_records = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = ChatRoom
+#         fields = ['id', 'user', 'health_records', 'recent_chat_history', 'summarized_chat_history', 'last_entered',
+#                   'entered', 'leaved']
+#
+#     def get_recent_chat_history(self, obj):
+#         return obj.chat_history
+#
+#     def get_summarized_chat_history(self, obj):
+#         return obj.summarized_chat_history
+#
+#     def get_health_records(self, obj):
+#         health_records = obj.health_records.all()
+#         serializer = HealthRecordImageSerializer(health_records, many=True)
+#         return serializer.data
--- health_records/serializers.py
+++ health_records/serializers.py
@@ -3,14 +3,15 @@ from .models import *
 
 
 class HealthRecordImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(allow_null=True, required=False)
     class Meta:
         model = HealthRecordImage
-        fields = ['user','id', 'image', 'ocr_text', 'created_at']
+        fields = ['user', 'image', 'ocr_text', 'created_at']
 
 
-class HealthRecordFolderSerializer(serializers.ModelSerializer):
-    images = HealthRecordImageSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = HealthRecordFolder
-        fields = ['user','id', 'created_at', 'images']
+# class HealthRecordFolderSerializer(serializers.ModelSerializer):
+#     images = HealthRecordImageSerializer(many=True, read_only=True)
+#
+#     class Meta:
+#         model = HealthRecordFolder
+#         fields = ['user','id', 'created_at', 'images']
