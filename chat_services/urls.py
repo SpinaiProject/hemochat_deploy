820cc0b568ae117bd723efd4af7ef1cbfd1c867d
hdsung98 <hdsung98@naver.com>
2024-05-13 16:49:24 +0900

    [Fix #1] Assistant API를 위한 API URL 맵핑

--- chat_services/urls.py
+++ chat_services/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create_assistant/', create_assistant_config, name='create_assistant_config'),
-    path('create_thread/', create_thread, name='create_thread'),
-    path('create_message/', create_message, name='create_message'),
+    #path('create_assistant/', create_assistant_config, name='create_assistant_config'),
+    # path('create_thread/', create_thread, name='create_thread'),
     # path('create_run/', create_run, name='create_run'),
-    path('enter_chatroom/<uuid:chatroom_id>/', enter_chatroom, name='enter_chatroom'),
+    # path('create_stream/<uuid:chatroom_id>/', create_stream, name='create_stream'),
+    # path('leave_chatroom/<uuid:chatroom_id>/', leave_chatroom, name='leave_chatroom'),
     path('create_chatroom/', create_chatroom, name='create_chatroom'),
-    path('delete_chatroom/<uuid:chatroom_id>/', delete_chatroom, name='delete_chatroom'),
-    path('create_stream/<uuid:chatroom_id>/', create_stream, name='create_stream'),
-    path('leave_chatroom/<uuid:chatroom_id>/', leave_chatroom, name='leave_chatroom'),
+    path('list_chatroom/', list_chatroom, name='list_chatroom'),
+    path('enter_chatroom/<str:chatroom_id>/', enter_chatroom, name='enter_chatroom'),
+    path('delete_chatroom/<str:chatroom_id>/', delete_chatroom, name='delete_chatroom'),
+    path('create_message/<str:chatroom_id>/', create_message, name='create_message'),
 ]
