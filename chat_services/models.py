27cd05b1dadc8ec5a733634126e743e42308efcd
hdsung98 <hdsung98@naver.com>
2024-05-13 19:52:34 +0900

    [Fix #1] Assistant API 전용으로 채팅방 모델 정의 변경 - OPEN AI 서버에서 대화내역을 관리할 예정이므로, 채팅방 대화내역 및 채팅방 입장여부 관련 필드들 삭제

--- chat_services/models.py
+++ chat_services/models.py
@@ -1,10 +1,8 @@
-import json
 import uuid
 
-from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.db.models import Manager
-
+from health_records.models import HealthRecordImage
 from hemochat_project import settings
 
 
@@ -12,33 +10,28 @@ class AssistantConfig(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     api_key = models.CharField(max_length=255)
-    model = models.CharField(max_length=255, default='gpt-3.5-turbo')
-    active = models.BooleanField(default=True)
+
+
+class ChatRoom(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='chatrooms')
+    health_records = models.ManyToManyField(HealthRecordImage, related_name='chatrooms')
+    title = models.CharField(max_length=255, blank=True)
+    chatroom_id = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
-
-
-class ChatThread(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='assistant_sessions')
-    thread_id = models.CharField(max_length=255, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects: Manager = models.Manager()
-    # title= models.CharField(max_length=255, blank=True)
 
 
 # 아래는 Assistant API Streaming이 지원되지 않을 때 사용할 모델들
-class ChatRoom(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    health_records = models.ManyToManyField('health_records.HealthRecordImage', related_name='chatrooms', blank=True)
-    chat_history = models.JSONField(blank=True, default=list)
-    summarized_chat_history = models.JSONField(blank=True, default=list)
-    last_entered = models.DateTimeField(auto_now=True)
-    entered = models.BooleanField(default=False)
-    leaved = models.BooleanField(default=True)
-    def __str__(self):
-        return f"ChatRoom {self.id}"
+# class ChatRoom(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     title = models.CharField(max_length=50)
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='chatrooms')
+#     health_records = models.ManyToManyField('health_records.HealthRecordImage', related_name='chatrooms', blank=True)
+#     chat_history = models.JSONField(blank=True, default=list)
+#     summarized_chat_history = models.JSONField(blank=True, default=list)
+#     last_entered = models.DateTimeField(auto_now=True)
+#     entered = models.BooleanField(default=False)
+#     leaved = models.BooleanField(default=True)
+#     def __str__(self):
+#         return f"ChatRoom {self.id}"
