e6b1a9dd4acedf77c196c816611bd6786a6d83ec
hdsung98 <hdsung98@naver.com>
2024-06-26 20:47:16 +0900

    [Fix #8] 비밀번호 관련 엔드포인트 수정(불필요 url도 삭제)

--- users/urls.py
+++ users/urls.py
@@ -4,33 +4,25 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('kakao/login/', kakao_login, name='kakao_login'),
-    #path('kakao/logout/', kakao_logout, name='kakao_logout'),
-    # path('kakao/callback/', kakao_callback, name='kakao_callback'),
-    # path('kakao/login/finish/', KakaoLogin.as_view(), name='kakao_login_todoclass'),
-
     path('google/login/', google_login, name='google_login'),
-    # path('google/callback/', google_callback, name='google_callback'),
-    # path('google/login/finish/', GoogleLogin.as_view(), name='google_login_todjango'),
 
     path('email/signup/', EmailSignupView.as_view(), name='email_signup'),
     path('email/login/', TokenObtainPairView.as_view(), name='email_login'),
     path('email/send-verification-code/', SendVerificationCodeAPIView.as_view(), name='send_verification_code'),
     path('email/verify-phone-number/', VerifyPhoneNumberAPIView.as_view(), name='verify_phone_number'),
-    # path('email/already_exist/', EmailAlreadyExistAPIView.as_view(), name='email_already_exist'),
 
     path('my-page/', MyPageView.as_view(), name='my-page'),
     path('user/update/', UserUpdateView.as_view(), name='user_update'),
     path('user/delete/', UserDeleteView.as_view(), name='user_delete'),
     path('user/refresh/', TokenRefreshView.as_view(), name='refresh'),
 
-
     path('find-email/send-verification-code/', SendEmailVerificationCodeAPIView.as_view(),
          name='find-email-verification-code'),
     path('find-email/return/', VerifyPhoneNumberAndReturnEmailAPIView.as_view(),
          name='find-email-return'),
 
-    # path('find-password/request/', RequestPhoneNumberForPassword.as_view(), name='find-password-request'),
-    path('find-password/send-verification-code/', VerifyPhoneNumberForPassword.as_view(), name='find-password-verify'),
-    path('find-password/reset/', PasswordResetView.as_view(), name='find-password-reset'),
+    path('find-password/send-verification-code/', SendPhoneNumberForPassword.as_view(), name='pwd_send_verification_code'),
+    path('find-password/verify-verification-code/', VerifyVerificationCodeView.as_view(), name='pwd_verify_code'),
+    path('find-password/reset/', PasswordResetView.as_view(), name='reset-password'),
 
 ]
